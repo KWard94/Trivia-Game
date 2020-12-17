216cd8f1e319ad8f66f38016579946fa975438a1
kward <pcward.k@gmail.com>
2020-12-17 15:40:25 -0700

    button event listener functional

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("My first real project begins here!")
+
+//first I will create functional buttons
+// i will start with just an "a" button to test functionality
+// I will create all four buttons and listen for click events
+
+//Later, I will have the buttons fill with the answers as variables
+function test() {
+    event.preventDefault()
+    console.log("I work!!!")
+}
+
+let aButton = document.querySelector("#answer-A")
+
+// aButton.addEventListener("click", alert("button works!"))
+aButton.addEventListener("click", test);
