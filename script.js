47e0a43aac3308e09fab22d35bdecfc50fe866a7
kward <pcward.k@gmail.com>
2020-12-20 11:39:08 -0700

    starting point for next button

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-//first I will create functional buttons
-// i will start with just an "a" button to test functionality
-// I will create all four buttons and listen for click events
-
-//Later, I will have the buttons fill with the answers as variables
-
-//^^DONE^^
-
-//I need to populate answers onto the buttons for a,b,c,d
 //I need to build out my array with more questions
 //i need to test the next question button so that it cycles through questions
 // 
@@ -38,31 +29,36 @@ let questionArray = [
         question: "What is the airspeed velocity of an unladen swallow?",
         answers: ["15km/hr", "20 km/hr", "African or European Swallow?", "25 km/hr"],
         correctAnsw: "African or European Swallow?",
-    }   
+    },
+    {
+        question: "test question 2",
+        answers: ["incorrect ans", "correct ans", "also incorrect", "third incorrect"],
+        correctAnsw: "correct ans",
+    },
 ]
 
-
+// for (i = 0; i <= questionArray.length; i++) {
+    //let i = variable?
 questionText.innerHTML = questionArray[0].question;
 
 aButton.value = questionArray[0].answers[0]
 bButton.value = questionArray[0].answers[1]
 cButton.value = questionArray[0].answers[2]
 dButton.value = questionArray[0].answers[3]
+//insert varible into above? (questionArray[variable].answers.....)
+//variable++?
+// }
 
-//NEED HELP TO WRITE FUNCTION FOR USER INPUT/CLICK ON CORRECT ANSWER
+//need to write next question button function, variable ++?
+//look up how to randomize entries in an array, could use to randomize answers
 
-// let userGuess = /*something*/
-// console.log(userGuess)
 
 function isCorrect(event) {
         console.log(event.target.value)
         if (event.target.value == questionArray[0].correctAnsw) {
-            console.log("Is correct")
+            console.log("Correct Answer Selected!")
             incrementScore();
         }
-    // if (questionArray[0].answers[userGuess] == questionArray[0].correctAnsw) {
-    //     console.log("Correct Answer!!!")
-    // }
 }
 
 var score = 0
@@ -76,13 +72,4 @@ function incrementScore () {
     console.log("score change")
 }
 
-function testClick() {
-    console.log("pretty sure working");
-    incrementScore();
-}
 
-//score increment function seems to work, but does not change score variable
-
-// aButton.onclick = function () {
-    //     console.log("A button clicked, did it really work???")
-    // }
